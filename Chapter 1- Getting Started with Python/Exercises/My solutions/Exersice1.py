63101053b376bc3c9417e72b75e75fc980a5e18c
GianKey247 <gianakiat@gmail.com>
2023-09-28 13:28:47 +0400

    Excersise1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/My solutions/Exersice1.py
@@ -0,0 +1,7 @@
+A = "Twinkle, twinkle, little star,"
+B = "	How I wonder what you are! "
+C = "		Up above the world so high, "
+D = "		Like a diamond in the sky. "
+E = "Twinkle, twinkle, little star, "
+
+print(f"{A}\n{B}\n{C}\n{D}\n{E}\n{B}")
