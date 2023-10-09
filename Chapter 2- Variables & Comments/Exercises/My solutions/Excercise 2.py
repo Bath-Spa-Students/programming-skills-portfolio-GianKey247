2ab36da4258424549db7ed621bc8ffcda47ac7c6
GianKey247 <gianakiat@gmail.com>
2023-10-09 21:16:45 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/My solutions/Excercise 2.py
@@ -0,0 +1,14 @@
+author="John Kassir"
+quote = "Yes. You aren't the only one to grow up on the killing fields.\n \
+War is a cruel parent, but an effective teacher.\n \
+Its final lesson is carved deep in my psyche:\n \
+That this world and all its people are diseased.\n \
+Free Will is a myth; Religion is a joke.\n \
+We are all pawns, controlled by something greater:\n \
+Memes. The DNA of the soul.\n \
+They shape our will.\n \
+They are the culture -- they are everything we pass on.\n \
+Expose someone to anger long enough, they will learn to hate.\n \
+They become a carrier... Envy, Greed, Despair...\n \
+All memes. All passed along."
+print (f'{author} once said,\n"{quote}"')
