be13d8ad96ab24843a71b91af5c142265a12f97d
GianKey247 <gianakiat@gmail.com>
2023-10-09 09:57:08 -0700

    Excercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/My solutions/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/My solutions/Exercise1.py
@@ -1,7 +1,7 @@
 A = "Twinkle, twinkle, little star,"
 B = "\tHow I wonder what you are! "
-C = "		Up above the world so high, "
-D = "		Like a diamond in the sky. "
+C = "\t\tUp above the world so high, "
+D = "\t\tLike a diamond in the sky. "
 E = "Twinkle, twinkle, little star, "
 
 print(f"{A}\n{B}\n{C}\n{D}\n{E}\n{B}")
