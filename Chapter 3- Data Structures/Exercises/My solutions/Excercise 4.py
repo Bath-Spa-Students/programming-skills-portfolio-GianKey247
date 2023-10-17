22568920c2997137cad0cf5bc18850060eb16d9c
GianKey247 <gianakiat@gmail.com>
2023-10-17 09:28:57 -0700

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 4.py
@@ -0,0 +1,9 @@
+Names = ["rick astley", "Takehito Koyasu", "mark hamil"]
+message = '*insert dinner invitation message here*'
+
+#another method
+#count =len(Names)
+#for index in range(count):
+#    print(Names[index],messages[index])
+for name in Names:
+    print(name.title() , message)
