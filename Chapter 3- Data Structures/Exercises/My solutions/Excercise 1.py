c1783261f149a19edb92819041461a70367f8f1b
GianKey247 <gianakiat@gmail.com>
2023-10-23 16:54:13 +0400

    Exercise 1.py

--- Chapter 3- Data Structures/Exercises/My solutions/Excercise 1.py
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 1.py
@@ -1,3 +1,8 @@
+#Using a list, contain a few of your friends names and print them out
+
+#List of names
 Names = ["migz","ola","nathan","laiba","aya"]
+
+#Getting all the names out of the list
 for name in Names:
     print(name)
