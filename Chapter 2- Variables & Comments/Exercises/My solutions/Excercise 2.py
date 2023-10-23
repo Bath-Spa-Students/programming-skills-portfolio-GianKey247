9f0ab8f9626e963aeedd4f8932f83181bc204aac
GianKey247 <gianakiat@gmail.com>
2023-10-23 16:53:46 +0400

    Exercise 2.py

--- Chapter 2- Variables & Comments/Exercises/My solutions/Excercise 2.py
+++ Chapter 2- Variables & Comments/Exercises/My solutions/Excercise 2.py
@@ -1,9 +1,11 @@
+#Set 2 variables as the author and its quote
+#using f strings to include, both variables and a message
+
 author="John Kassir"
 quote = "Yes. You aren't the only one to grow up on the killing fields.\n \
 War is a cruel parent, but an effective teacher.\n \
 Its final lesson is carved deep in my psyche:\n \
 That this world and all its people are diseased.\n \
-Free Will is a myth; Religion is a joke.\n \
 We are all pawns, controlled by something greater:\n \
 Memes. The DNA of the soul.\n \
 They shape our will.\n \
