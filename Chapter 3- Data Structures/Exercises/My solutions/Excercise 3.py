81a2c1906e23c8f66c9a248263c0bf5d511efcc8
GianKey247 <gianakiat@gmail.com>
2023-10-23 16:54:34 +0400

    Exercise 3.py

--- Chapter 3- Data Structures/Exercises/My solutions/Excercise 3.py
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 3.py
@@ -1,14 +1,13 @@
+#List of transportation
 Names = ["Golf","Mclaren F1","Supra","Skyline","Ford GT"]
+
+#List of Messages
 messages = ['is going to my main car of chice for daily means',
             'is the best track car that i would like to drive atleast once',
             'is one of the best tuner cars out there but its extremely rare and expensive',
             'have a similar case to the supra',
             'is one of my favourite child hood cars']
 
-#another method
-#count =len(Names)
-#for index in range(count):
-#    print(Names[index],messages[index])
-
+#Prints out the names and corresponding messages
 for index, name in enumerate(Names):
     print(Names[index],messages[index])
