27c7ff1ff9612c2ba76e356112d6a5a25d4b1c13
GianKey247 <gianakiat@gmail.com>
2023-10-23 17:28:34 +0400

    Exercise 7.py

--- Chapter 3- Data Structures/Exercises/My solutions/Excercise 7.py
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 7.py
@@ -1,11 +1,20 @@
+#List out loactions of interest
+#Follow sorting instructions in the Excercise 7
+
 Locations = ["Italy", "Egypt", "America", "England", "Japan"]
 print(f"Original List of locations: {Locations}")
+
+#Sorts elements in list as alphabetical order
 sorted_Locations = sorted(Locations)
 print(f"Sorted list of location: {sorted_Locations}")
 print(f"Original list of locations: {Locations}")
+
+#Reverses order of elements inside the list
 reversed_Locations = sorted(Locations, reverse=True)
 print(f"Reversed Sorted list of locations {reversed_Locations}")
 print(f"Original list of locations: {Locations}")
+
+#Reverses order of elements inside the list
 Locations.reverse()
 print(Locations)
 Locations.reverse()
