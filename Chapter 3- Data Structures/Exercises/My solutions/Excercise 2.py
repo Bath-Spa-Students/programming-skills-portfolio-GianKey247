1c496759fec3f6c67a1f89fa491b8dc4e6826716
GianKey247 <gianakiat@gmail.com>
2023-10-23 16:54:26 +0400

    Exercise 2.py

--- Chapter 3- Data Structures/Exercises/My solutions/Excercise 2.py
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 2.py
@@ -1,14 +1,13 @@
+#List of names
 Names = ["migz","ola","nathan","laiba","aya"]
+
+#List of Messages
 messages = ['he will say "make it stop" when you have a problem',
             'she good',
             'sensable compared the rest of them',
             'she has more work experience compared to the rest of us',
             'she is always sleepy']
 
-#another method
-#count =len(Names)
-#for index in range(count):
-#    print(Names[index],messages[index])
-
+#Prints out the names and corresponding messages
 for index, name in enumerate(Names):
     print(Names[index],messages[index])
