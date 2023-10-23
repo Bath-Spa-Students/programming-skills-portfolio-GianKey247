97bd62307ab78b5df9c2961bec0033454eed359f
GianKey247 <gianakiat@gmail.com>
2023-10-23 16:53:23 +0400

    Exercise 5.py

--- Chapter 1- Getting Started with Python/Exercises/My solutions/Exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/My solutions/Exercise5.py
@@ -1,2 +1,4 @@
+#Using input, get the users input for the radius and use it to find the area of a circle
+
 number1= int(input("Give radius:"))
-print(number1*number1*3.14)
+print(f"If the radius of the circle is {number1}\nThen the area will be {number1*number1*3.14}")
