0c22922f71b48abff1ff9251dea9f4cdb8e87322
GianKey247 <gianakiat@gmail.com>
2023-10-23 17:27:56 +0400

    Exercise 4.py

--- Chapter 3- Data Structures/Exercises/My solutions/Excercise 4.py
+++ Chapter 3- Data Structures/Exercises/My solutions/Excercise 4.py
@@ -1,9 +1,10 @@
-Names = ["rick astley", "Takehito Koyasu", "mark hamil"]
-message = '*insert dinner invitation message here*'
+#List out the names, you would like to invite to a dinner party
+#Print out a personalised message for each element
 
-#another method
-#count =len(Names)
-#for index in range(count):
-#    print(Names[index],messages[index])
+#List of Names
+Names = ["rick astley", "Takehito Koyasu", "mark hamil"]
+message = 'is invited to your dinner party'
+
+#Prints out the message for each person in the list
 for name in Names:
     print(name.title() , message)
