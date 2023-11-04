0ded4dd7a9cea66fd88b62df463b6cdf5cebfaf5
GianKey247 <gianakiat@gmail.com>
2023-11-04 14:30:58 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/My Solutions/Exercise 1.py
@@ -0,0 +1,15 @@
+#Using dictionaries
+#Store information of a individual on First, Last Name, Age and where they live
+person_Data = {
+    'First Name': 'Gian'
+    ,
+    'Last Name': 'Akiat'
+    ,
+    'Age': 18
+    ,
+    "Lives in": 'Ras al khaimah'
+}
+print(person_Data['First Name'])
+print(person_Data['Last Name'])
+print(person_Data['Age'])
+print(person_Data['Lives in'])
