fd539625082eb4f8b948dee0cabd66ee71c65cb8
GianKey247 <gianakiat@gmail.com>
2023-11-04 14:33:03 +0400

    Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/My Solutions/Exercise 2.py
@@ -0,0 +1,6 @@
+#Use a function to display a message
+#Designate a parameter in the message and print it out
+def favorite_book(title):
+
+    print(f"My favourite book is {title}")
+favorite_book("One Punch Man")
