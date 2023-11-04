6559920619d97bf670b065e10438123b1c64b31a
GianKey247 <gianakiat@gmail.com>
2023-11-04 14:33:17 +0400

    Exercise 4.py

---
+++ Chapter 7- Functions/Exercises/My Solutions/Exercise 4.py
@@ -0,0 +1,7 @@
+# Define a function called make_shirt with two parameters: size and message.
+# Display 2 messages, they have different size but same message.
+def make_shirt(size, message="I love Python"):
+    # Print the size and message of the shirt.
+    print(f'shirt size is {size} and its message is {message}')
+make_shirt("large")
+make_shirt("medium")
