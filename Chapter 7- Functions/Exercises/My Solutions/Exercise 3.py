d85234b7e3f82c78e881a47a8793d997049794fb
GianKey247 <gianakiat@gmail.com>
2023-11-04 14:33:10 +0400

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/My Solutions/Exercise 3.py
@@ -0,0 +1,5 @@
+#Use a function to display a message
+#Designate 2 parameters in the message and print it out
+def make_shirt(size, message):
+    print(f'shirt size is {size} and its message is {message}')
+make_shirt("Large", "hi")
