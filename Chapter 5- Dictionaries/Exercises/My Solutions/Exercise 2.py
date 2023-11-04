78721e809d7db507063b70effab8876d35e65ce3
GianKey247 <gianakiat@gmail.com>
2023-11-04 14:31:05 +0400

    Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/My Solutions/Exercise 2.py
@@ -0,0 +1,18 @@
+#Using dictionaries
+#Store information of lessons that you have learnt and display its key and value
+python_Lessons = {
+    'String': 'It mainly contains characters, like the aplhabets'
+    ,
+    'Float': 'It contains numbers with decimals'
+    ,
+    'Integer': 'It contains only whole numbers'
+    ,
+    "F string": 'Allows functions to be placed in the string'
+    ,
+    "If statement": 'Uses conditions to check the input and use appropriate output'
+}
+print("String",python_Lessons['String'])
+print("Float", python_Lessons['Float'])
+print("Integer", python_Lessons['Integer'])
+print("F String", python_Lessons['F string'])
+print("If string", python_Lessons['If statement'])
