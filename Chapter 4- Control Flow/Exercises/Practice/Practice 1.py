9e5c82ea870a8e55ec406e9a16e001808fdd7b0b
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:56:49 +0400

    Practice 1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice/Practice 1.py
@@ -0,0 +1,11 @@
+#set y, z as 20, 40 respectively
+#set if to check if the varables are greater than 100
+y = 20
+z = 40
+if y>100:
+    print("Y is greater")
+print("Y is not greater")
+
+if z>100:
+    print("Z is greater")
+print("Z is not greater")
