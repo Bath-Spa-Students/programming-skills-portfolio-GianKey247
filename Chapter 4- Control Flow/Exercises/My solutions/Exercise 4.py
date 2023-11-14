27fcf5f9620d394e67455ccc8762038427912374
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:56:29 +0400

    Exercise 4.py

---
+++ Chapter 4- Control Flow/Exercises/My solutions/Exercise 4.py
@@ -0,0 +1,28 @@
+# The age will determine the stages of growth
+
+
+age = 1
+
+#If the age is less than 2, the individual is a baby
+if age<2:
+    print("the indivual is a baby")
+
+#If the age is more than 2 and less than 4 the individual is a toddler
+elif age>=2 and age<4:
+    print("the indivual is a toddler")
+
+#If the age is more than 4 and less than 13 the individual is a kid
+elif age>=4 and age<13:
+    print("the indivual is a kid")
+
+#If the age is more than 13 and less than 20 the individual is a teenager
+elif age>=13 and age<20:
+    print("the indivual is a teenager")
+
+#If the age is more than 20 and less than 65 the individual is a adult
+elif age>=20 and age<65:
+    print("the indivual is a adult")
+
+#If the age is more than 65, the individual is a elder
+elif age>=65:
+    print("the indivual is a elder")
