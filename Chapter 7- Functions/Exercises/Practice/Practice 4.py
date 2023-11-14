a71833ff5b0e553394c4eb73d8cba671f88cb530
GianKey247 <gianakiat@gmail.com>
2023-11-14 18:00:24 +0400

    Practice 4.py

---
+++ Chapter 7- Functions/Exercises/Practice/Practice 4.py
@@ -0,0 +1,7 @@
+#make a function, set a perameter
+#using the peramter as a radius
+#calculate the area of a circle
+def area_Circle(rad):
+    area = rad*rad*3.14
+    print (f"the area is of the circle is {area}")
+area_Circle(5)
