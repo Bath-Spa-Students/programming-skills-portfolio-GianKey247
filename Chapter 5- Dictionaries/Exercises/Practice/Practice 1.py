a5fabb508c6b6081bb76c0426059fcbea2cffaf6
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:59:10 +0400

    Practice 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice/Practice 1.py
@@ -0,0 +1,14 @@
+#Make a dictionary to store personal information
+personal_Information = {
+    "Name" : "Gian"
+    ,
+    "Last Name" : "Akiat"
+    ,
+    "Age" : 18
+    ,
+    "Day of bith" : 3
+    ,
+    "Month of birth" : "August"
+    ,
+    "Year of birth" : 2005
+    }
