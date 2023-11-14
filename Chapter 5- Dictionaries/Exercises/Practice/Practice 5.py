60f4337e1be4416f4aa208003e133e4636e1a963
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:59:32 +0400

    Practice 5.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice/Practice 5.py
@@ -0,0 +1,36 @@
+personal_Information_1 = {
+    "Gian":
+    {
+    "Name" : "Gian"
+    ,
+    "Last Name" : "Akiat"
+    ,
+    "Age" : 18
+    ,
+    "Day of bith" : 3
+    ,
+    "Month of birth" : "August"
+    ,
+    "Year of birth" : 2005
+    }
+    }
+
+personal_Information_2 = {
+    "Giennelle":
+    {
+    "Name" : "Giennelle"
+    ,
+    "Last Name" : "Akiat"
+    ,
+    "Age" : 16
+    ,
+    "Day of bith" : 30
+    ,
+    "Month of birth" : "November"
+    ,
+    "Year of birth" : 2006
+    }
+    }
+
+personal_Information_1.update(personal_Information_2)
+print(personal_Information_1)
