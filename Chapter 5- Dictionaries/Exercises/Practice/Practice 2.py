00c554db9d14d7d7512d947ffe1547d7df0efc48
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:59:14 +0400

    Practice 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice/Practice 2.py
@@ -0,0 +1,16 @@
+#Using loop, print out the keys in the dictionary
+personal_Information = {
+    "Name" : "Gian"
+    ,
+    "Last Name" : "Akiat"
+    ,
+    "Age" : 18
+    ,
+    "Day of bith" : 3
+    ,
+    "Month of birth" : "August"
+    ,
+    "Year of birth" : 2005
+    }
+for information in personal_Information.keys():
+    print(information)
