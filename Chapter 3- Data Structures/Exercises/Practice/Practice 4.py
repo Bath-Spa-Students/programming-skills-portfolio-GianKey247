ad57b52bc78efaeeee65e5bf685ca9f37eef8745
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:55:38 +0400

    Practice 4.py

---
+++ Chapter 3- Data Structures/Exercises/Practice/Practice 4.py
@@ -0,0 +1,8 @@
+#lets say, one list have 500 elements
+#duplicate the elements into another list
+numbers1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+numbers2 = []
+for number in numbers1:
+    numbers2.append(number)
+print(numbers1)
+print(numbers2)
