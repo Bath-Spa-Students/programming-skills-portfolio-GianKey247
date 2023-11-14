d10e27255b0e65b2e2966873aeadc3d3c62d073e
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:58:38 +0400

    Practice 4.py

---
+++ Chapter 4- Control Flow/Exercises/Practice/Practice 4.py
@@ -0,0 +1,10 @@
+#set a varable "speed"
+#using if staements
+#check if the speed is between 24 - 56
+#if it is in range, print "speed is nomral"
+#else "speed is abnormal"
+speed = 30
+if speed >= 24 and speed <=56:
+    print("speed is nomral")
+else:
+    print("speed is abnormal")
