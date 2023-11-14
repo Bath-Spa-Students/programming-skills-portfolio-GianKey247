c4de5252b1d0cad83e082c8b7726928faa537d5a
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:55:23 +0400

    Practice 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice/Practice 5.py
@@ -0,0 +1,8 @@
+#make a list of fruits
+#assign x, y, z with the 3 elements in the list
+#print out the variables
+fruits = ["apple", "watermelon", "melon"]
+x = fruits[0]
+y = fruits[1]
+z = fruits[2]
+print (x, y, z)
