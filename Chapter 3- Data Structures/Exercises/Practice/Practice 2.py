83e1550cd07b6d6c835d3f87b520bff31d9f943b
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:55:30 +0400

    Practice 2.py

---
+++ Chapter 3- Data Structures/Exercises/Practice/Practice 2.py
@@ -0,0 +1,8 @@
+#Make a empty list
+#Add elements from the question into the list 
+names = []
+names.append('Einstein')
+names.append('Newton')
+names.append('Copernicus')
+names.append('Kepler')
+print(names)
