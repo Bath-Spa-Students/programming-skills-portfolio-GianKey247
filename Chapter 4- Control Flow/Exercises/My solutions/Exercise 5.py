519d47c772a53c0429beb6461220694b67aae43c
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:56:35 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/My solutions/Exercise 5.py
@@ -0,0 +1,15 @@
+#Make a list of favourite fruits
+#Use if to check if the list, have a specific item
+
+favorite_fruits = ["Banana", "Grapes", "Apples"]
+
+if "Cherry" in favorite_fruits:
+    print("You like Cherry")
+if "Banana " in favorite_fruits:
+    print("You like Banana")
+if "Grapes" in favorite_fruits:
+    print("You like Grapes")   
+if "Lyche" in favorite_fruits:
+    print("You like Lyche")
+if "Melon" in favorite_fruits:
+    print("You like Melon")
