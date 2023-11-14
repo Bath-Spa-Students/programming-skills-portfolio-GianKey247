222a33ce78d34f44c9314bda83c8408457f39aaa
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:55:34 +0400

    Practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/Practice/Practice 3.py
@@ -0,0 +1,7 @@
+#Using the list from the previous pratice
+#use a loop to print out all the elements in the list
+names = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+for name in names:
+    print(name)
+
+    
