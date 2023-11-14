f3e20cab2d4d9526e991ae85caf8a8589591e2eb
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:56:25 +0400

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/My solutions/Exercise 3.py
@@ -0,0 +1,47 @@
+#If the alien is green, then add 5 points
+#If the alien is yellow, then add 10 points
+#If the alien is red, then add 15 points
+
+#Alien color is Green
+alien_color = "Green"
+
+points = 0
+if alien_color == "Green":
+    points = points + 5
+elif alien_color == "Yellow":
+    points = points + 10
+elif alien_color == "Red":
+    points = points + 15
+else:
+    print("You missed")
+print(f"You earnt {points} points")
+
+
+#Alien color is Yellow
+alien_color = "Yellow"
+
+points = 0
+if alien_color == "Green":
+    points = points + 5
+elif alien_color == "Yellow":
+    points = points + 10
+elif alien_color == "Red":
+    points = points + 15
+else:
+    print("You missed")
+print(f"You earnt {points} points")
+
+
+#Alien color is Red
+alien_color = "Red"
+
+points = 0
+if alien_color == "Green":
+    points = points + 5
+elif alien_color == "Yellow":
+    points = points + 10
+elif alien_color == "Red":
+    points = points + 15
+else:
+    print("You missed")
+print(f"You earnt {points} points")
