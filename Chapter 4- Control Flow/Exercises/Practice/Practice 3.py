1c30adee0a7a76c08524450b58a57b52eeb6fd03
GianKey247 <gianakiat@gmail.com>
2023-11-14 17:56:57 +0400

    Practice 3.py

---
+++ Chapter 4- Control Flow/Exercises/Practice/Practice 3.py
@@ -0,0 +1,12 @@
+#set 2 variables as amount 1 and amount 2
+#use if to check of amount 1 is > 10
+#use if to check of amount 2 is < 100
+#print out which variable is greater than the other
+amount1 = 11
+amount2 = 99
+if amount1 > 10 and amount2 <100:
+    if amount1 > amount2:
+        print (f"{amount1} > {amount2}")
+    else:
+        print (f"{amount1} < {amount2}")
+    
